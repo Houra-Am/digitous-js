42eeeae108aaf1778c0a0e1508411006d496bf1d
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2020-12-07 14:56:39 +0100

    js exos from 1-7 exo 8 working on it

---
+++ jour01.js
@@ -0,0 +1,26 @@
+console.log("Hello World !");
+
+var test = "My name is Houra";
+console.log(test);
+
+var name = "Houra";
+console.log("Nice to meet you " + name + "!");
+
+var testLength = "I'm very long !";
+console.log(testLength.length);
+
+var food = "Croissant is meh";
+console.log(food.replace("meh", "so good"));
+
+var basic = "This is cool";
+var basicUp = basic.toUpperCase();
+console.log(basicUp);
+
+var basicDown = basic.toLocaleLowerCase();
+console.log(basicDown);
+
+console.log(basic, basicUp, basicDown);
+
+var word = "banana";
+var letters = word.split("");
+console.log(letters);
