e65b8f9a5a71f3100ad4314bd799106d43138630
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2020-12-07 15:01:16 +0100

    exo 8

--- jour01.js
+++ jour01.js
@@ -24,3 +24,7 @@ console.log(basic, basicUp, basicDown);
 var word = "banana";
 var letters = word.split("");
 console.log(letters);
+
+var age = "34";
+var template = `I'm ${age} years old`;
+console.log(template);
