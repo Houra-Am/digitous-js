28a6665f4bd3dedbee28a29e2fe6d80c4f126a21
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2020-12-09 14:30:55 +0100

    exercise 1 to 4

---
+++ jour03.js
@@ -0,0 +1,25 @@
+// 01 - Array
+
+var fruits = ["mango", "lemon", "blueberry"];
+console.log(fruits);
+console.table(fruits);
+
+// 02 - Access
+
+var ingredients = ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
+
+// 03 - Access
+
+var objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+console.log(objects);
+
+// 04 - Order
+
+var numbers = [4, 10, 8, 12, 6];
+numbers.reverse();
+console.log(numbers);
+
+// 05 - Boucle
