683dbac281a39b385773a7a0bf3e21ec56931324
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2021-01-04 14:43:48 +0100

    exo2

--- jour07.js
+++ jour07.js
@@ -9,3 +9,17 @@ fs.readFile("./jour07.txt", function(err, data) {
     console.log("The file contains these info: " + data.toString());
 
 });
+
+// 02 - Map Double
+/*
+- Créez une variable `array` contenant un tableau qui contiendra les valeurs `1`, `2`, `3`, `4` et `5`
+- Créez une variable `double` qui appellera la méthode `.map()` pour contenir les doubles des valeurs de `array`
+- Affichez les valeurs de `double` dans la console
+*/
+
+var array = [1, 2, 3, 4];
+var double = array.map(function(number) {
+    return number * 2;
+});
+
+console.log(double);
