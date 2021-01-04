19bbb2f7912fc1367ffd395caad4f1ad7836872e
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2021-01-04 15:11:00 +0100

    exo3

--- jour07.js
+++ jour07.js
@@ -1,3 +1,4 @@
+// 01
 var fs = require("fs");
 
 fs.readFile("./jour07.txt", function(err, data) {
@@ -7,15 +8,9 @@ fs.readFile("./jour07.txt", function(err, data) {
     }
 
     console.log("The file contains these info: " + data.toString());
-
 });
 
 // 02 - Map Double
-/*
-- Créez une variable `array` contenant un tableau qui contiendra les valeurs `1`, `2`, `3`, `4` et `5`
-- Créez une variable `double` qui appellera la méthode `.map()` pour contenir les doubles des valeurs de `array`
-- Affichez les valeurs de `double` dans la console
-*/
 
 var array = [1, 2, 3, 4];
 var double = array.map(function(number) {
@@ -23,3 +18,22 @@ var double = array.map(function(number) {
 });
 
 console.log(double);
+
+// 03 - Map Names
+
+var longNames = [{
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+];
+
+var shortNames = longNames.map(function(fullName) {
+    return "name: " + fullName.firstName + " " + fullName.lastName;
+
+});
+
+console.log(shortNames);
